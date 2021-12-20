0a3ee433e0185b3d87f2a38c0fea718c58a24512
Usman Danbaba <usmandanbaba258@yahoo.com>
2021-12-20 12:49:55 +0100

    add initial commit

--- blockvoke.js
+++ blockvoke.js
@@ -6,8 +6,10 @@ const index = require('./node_modules/bitcore-lib/index')
 const bitcore = require('bitcore-lib')
 const axios = require("axios");
 var privateKey = new bitcore.PrivateKey();
+console.log('private key is', privateKey)
 
 var address = privateKey.toAddress();
+console.log('Address is', address)
 
 // Generate a address from a SHA256 hash
 
@@ -86,6 +88,7 @@ var address = new bitcore.Address(publicKeys, requiredSignatures);
 var privateKeys = [
   new bitcore.PrivateKey('91avARGdfge8E4tZfYLoxeJ5sGBdNJQH4kvjJoQFacbgwmaKkrx')
 ];
+console.log('privateeee keyyy isss', privateKeys)
 var publicKeys = privateKeys.map(bitcore.PublicKey);
 var address = new bitcore.Address(publicKeys, 1); // 1 of 2
 
@@ -101,3 +104,22 @@ var transaction = new bitcore.Transaction()
     .from(utxo, publicKeys, 1)
     .to('mtoKs9V381UAhUia3d7Vb9GNak8Qvmcsme', 20000)
     .sign(privateKeys);
+    console.log('transactionnnn isssss', transaction)
+
+    var btcadd = "n175oPAUkQxX8RutSgDffAzgSUruMZzSvE"
+    var prvkey = "91oqwCFiAP22r9pnuMQYBVztVPwhEPRHciBLkfdftPB1y2RsEAh"
+
+    let sendBitcoin = (recieverAddress, amountToSend)=>{
+      const sochain_network = "BTCTEST"; // the Testnet network for sochain
+      const privateKey = `93F2mUJPKbXW8Q9cMNz4ZmpsjgTbNjrMeCaUesTPE7k1DFhSmnk`;// your privateKey -- the one we just generated
+    
+    /* your bitcoin address. The one you want to send funds from -- the one we just generated */
+      const sourceAddress = `mtVE8anM63kQcgKUC6oQQD9K6xiV4wsr7q`; 
+    /**
+    because the outputs come in satoshis, and 1 Bitcoin is equal to 100,000,000 satoshies, we'll multiply the amount of bitcoin by 100,000,000 to get the value in satoshis.
+    */
+      const satoshiToSend = amountToSend * 100000000; 
+      let fee = 0; 
+      let inputCount = 0;
+      let outputCount = 2; // we are going to use 2 as the output count because we'll only send the bitcoin to 2 addresses the receiver's address and our change address.
+    }
